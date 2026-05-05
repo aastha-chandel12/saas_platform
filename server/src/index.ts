b3ffb37358215dc8b7b81849a47cfa5f2c43bd79
aastha-chandel12 <aastha1003.be21@chitkarauniversity.edu.in>
2026-05-05 18:34:23 +0530

    update cors for deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173', // Vite default port
+  origin: "*",
   credentials: true
 }));
 
