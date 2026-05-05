0d4b024acb3fb335c851798e437f4ae5bcd09b33
aastha-chandel12 <aastha1003.be21@chitkarauniversity.edu.in>
2026-05-06 01:08:01 +0530

    fix cors for vercel domain

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "*",
+  origin: [
+    "http://localhost:5173",
+    "https://saas-platform-alpha.vercel.app"
+  ],
   credentials: true
 }));
 
