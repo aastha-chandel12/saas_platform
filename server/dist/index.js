a6b10cafa270e4352974446d86914b96ab57b451
aastha-chandel12 <aastha1003.be21@chitkarauniversity.edu.in>
2026-05-06 01:09:19 +0530

    fix cors credentials issue

--- server/dist/index.js
+++ server/dist/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: "https://saas-platform-alpha.vercel.app",
     credentials: true
 }));
 app.use('/api/users', userRoutes);
