f25137dfc3800c3be8868d042897bd439d1fa323
aastha-chandel12 <aastha1003.be21@chitkarauniversity.edu.in>
2026-05-06 18:00:41 +0530

    commit

--- server/src/utils/generateToken.ts
+++ server/src/utils/generateToken.ts
@@ -9,7 +9,7 @@ const generateToken = (res: Response, userId: string) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'lax',
+    sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'none',
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
